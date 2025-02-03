f0b6888ce4cf0a04e5216a8c717686830296301d
Solmaz Purser <solmaz@hotmail.co.uk>
2025-02-03 21:43:03 +0000

    added notes

--- Build_a_pyramid_generator/pyramid.js
+++ Build_a_pyramid_generator/pyramid.js
@@ -3,13 +3,13 @@
 
 // To achieve this, you will use the .repeat() method available to strings. This method accepts a number as an argument, 
 // specifying the number of times to repeat the target string. For example, using .repeat() to generate the string "Code! Code! Code!":
-
+// Run code using: node pyramid.js
 const character = "#";
 const count = 8;
 const rows = [];
 
 for (let i = 0; i < count; i = i + 1) {
-    rows.push(character.repeat(i));
+    rows.push(character.repeat(i + 1));
   }
 
   let result = ""
