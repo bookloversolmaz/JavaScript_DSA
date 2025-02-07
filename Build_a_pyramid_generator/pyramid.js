8be4905391c043765218973b65e19516896ad486
Solmaz Purser <solmaz@hotmail.co.uk>
2025-02-07 19:30:24 +0000

    changed name of files to better reflect content

--- Build_a_pyramid_generator/pyramid.js
+++ Build_a_pyramid_generator/pyramid.js
@@ -1,29 +1,27 @@
-// Now you have a series of # characters, but the pyramid shape is still missing. Fortunately, the i variable 
-// represents the current "row" number in your loop, enabling you to use it for crafting a pyramid-like structure.
+// Use code developed in triangle.js
 
-// To achieve this, you will use the .repeat() method available to strings. This method accepts a number as an argument, 
-// specifying the number of times to repeat the target string. For example, using .repeat() to generate the string "Code! Code! Code!":
-// Run code using: node pyramid.js
 const character = "#";
 const count = 8;
 const rows = [];
 
-for (let i = 0; i < count; i = i + 1) {
-    rows.push(character.repeat(i + 1));
+function padRow(rowNumber, rowCount) { // To create a row, need to know which row it is on and the overall number of rows. Without rowCount, it will carry on creating forever.
+    return character.repeat(rowNumber);  // This will return the character multiplied by the row number. Note, cannot do character * rowNumber
+  }
+//   Pass i + 1 and count as the arguments to your padRow call. Like parameters, arguments are separated by a comma.
+
+  for (let i = 0; i < count; i = i + 1) {
+    rows.push(padRow(i + 1, count))
   }
 
-  let result = ""
+  function padRow(rowNumber, rowCount) {    // This adds a single space to the beginning and end of the repeats character string
+    return " " + character.repeat(rowNumber) + " ";
+  }
 
-for (const row of rows) {
-  result = result + row + "\n";
-}
-
-console.log(result);
-// This prints: 
-// #
-// ##
-// ###
-// ####
+// To create a three row pyramid, consider the image below. The blank spaces (represented by dots) follow a pattern: rowCount - rowNumber
+// ··#··    The no of rows is 3 (rowCount) and the rowNumber is 1, 3 -1 = 2 spaces.
+// ·###·
 // #####
-// ######
-// #######
+// The code above is amended to take the number of spaces for each row into account
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ". repeat(rowCount - rowNumber);
+  }
