8872b4d6e91ddec838edb191013cd8859d9d9575
Solmaz Purser <solmaz@hotmail.co.uk>
2025-02-12 18:14:52 +0000

    created inverted triangle using shift

--- Build_a_pyramid_generator/inverted_triangle_refactored.js
+++ Build_a_pyramid_generator/inverted_triangle_refactored.js
@@ -13,3 +13,23 @@
 // 1
 // 3
 // [ 5, 2, 3 ]
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+// unshift adds new rows to the beginnning of the array
+for (let i = count; i > 0; i--) {
+    rows.unshift(padRow(i, count));   // push the result of calling padRow with your i and count variables to your rows array
+  }
+
+  let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
