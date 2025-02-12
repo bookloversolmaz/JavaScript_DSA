f84487e732cd0d957b6280dc70aaea8270de0459
Solmaz Purser <solmaz@hotmail.co.uk>
2025-02-12 17:21:41 +0000

    completed inverted triangle

---
+++ Build_a_pyramid_generator/inverted_triangle.js
@@ -0,0 +1,21 @@
+// As the loop is in the opposite direction, the loop needs to run while i is greater than 0.
+// As each iteration subtracts one from i, the iteration statement is i - 1
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = count; i > 0; i = i - 1) {
+    rows.push(padRow(i, count));   // push the result of calling padRow with your i and count variables to your rows array
+  }
+
+  let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
