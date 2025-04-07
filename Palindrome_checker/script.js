a1bb7a2a7e30bab8d3d81f3df7fe5d89c8191d41
Solmaz Purser <solmaz@hotmail.co.uk>
2025-04-07 20:54:38 +0100

    developing js for palindrome checker 3

--- Palindrome_checker/script.js
+++ Palindrome_checker/script.js
@@ -5,42 +5,43 @@
 // Create below variables to represent user input, outputs and interactions
 const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
-const UserResult = document.getElementById("result");
+const userResult = document.getElementById("result");
 
 const cleanInput = (userText) => {
-    // Remove punctuation, spaces and symbols
-    const regex = /[^a-zA-Z0-9]/g;
-    // Replace regex characters with nothing and change to all lowercase
-    return userText.replace(regex, '').toLowerCase();
+    // Remove punctuation, spaces and symbols and change to all lowercase
+    return userText.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 };
 
 //  when user clicks on the#check-btn element without entering a value into the #text-input element, 
 // an alert should appear with the text "Please input a value."
 const checkEmptyInput = (userText) => {
-   if (userText === "")
-       alert("Please input a value.");
-       return false;
+    // trim removes whitepsaces from both ends of the string so " " becomes "" need to check for nothing rather than empty space
+    if (userText.trim() === "") {
+        alert("Please input a value.");
+        return true; // true means input IS empty
+    }
+        return false;
 };
 const isItAPalindrome = () => {
-    // Find the value of the DOM element that contains the user input
-    const userText = textInput.value; //Get value of input field
     // Implement cleanedInput
     const cleanedString = cleanInput(userText);
     // Reverse the cleaned string
-    const reversedString = cleanInput.split("").reverse().join("");
+    const reversedString = cleanedString.split("").reverse().join("");
     // Compare both iterations if identical return string "$`textInput` is a palindrome"
     if (cleanedString === reversedString) {
         return `<span class="user-result">"${textInput.value} is a palindrome"</span>`
     } else {
         // If not identical return string "$`textInput` is not a palindrome"
-       return `<span class="user-result">"${textInput.value} is not a palindrome"</span>`
+        return `<span class="user-result">"${textInput.value} is not a palindrome"</span>`
     } 
 };
 
 // Process the program. When user clicks add button run functions above
 checkButton.addEventListener("click", () => {
-    // check if string is empty
-    checkEmptyInput();
-    // run isitpalindrome
-    isItAPalindrome();
+    const userText = textInput.value; // Get fresh input on click
+    // check if string is empty 
+    if (checkEmptyInput(userText)) return;
+    // Process the message
+    const resultMessage = isItAPalindrome(userText);
+    userResult.innerHTML = resultMessage; // Display result in the DOM
 });
